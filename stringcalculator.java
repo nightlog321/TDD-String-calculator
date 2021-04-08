9f6de92e5b063288efbb7d5af7a77d32346234cc
nightlog321 <36795018+nightlog321@users.noreply.github.com>
2021-04-08 16:27:06 +0530

    Add files via upload
    for an empty string,return 0.

---
+++ stringcalculator.java
@@ -0,0 +1,9 @@
+package check;
+public class stringcalculator {
+
+	public static int add(String t){
+		if(t.equals("")){
+			return 0;
+		}
+}
+}
---
+++ teststringcalculator.java
@@ -0,0 +1,13 @@
+package check;
+import org.junit.Test;
+public class teststringcalculator {
+
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("check.teststringcalculator");
+    }
+
+	@Test
+	public void nulltest() {
+		assertEquals(0, stringcalculator.add(""));
+	}
+}
