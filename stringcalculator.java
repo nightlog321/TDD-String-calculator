7a83f99bf8e2558dbf8d4815bbd1791c2894e25a
nightlog321 <36795018+nightlog321@users.noreply.github.com>
2021-04-08 16:30:52 +0530

    Update stringcalculator.java

--- stringcalculator.java
+++ stringcalculator.java
@@ -1,8 +1,8 @@
 package check;
 public class stringcalculator {
 
-	public static int add(String t){
-		if(t.equals("")){
+	public static int add(String t){ 
+		if(t.equals("")){ //no number added
 			return 0;
 		}
 }
